da13a3e9a04cec5b38f2c48246433a25ca49d3a8
will-oracions <oracions.dev@gmail.com>
2023-11-24 22:57:15 +0100

    save

--- app.py
+++ app.py
@@ -2,14 +2,24 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/tp")
 def hello_world():
     return render_template('index.html')
 
 @app.route("/", methods=['POST'])
 def get_data():
     username = request.form['username']
-    return render_template('index.html', username=username)
+    members = ["Will", "Jeremy", "Perez", "Josephine", "Princesse"]
+    isCold = True
+
+    print(members)
+    
+    return render_template(
+        'index.html',
+        username=username,
+        members=members,
+        isCold=isCold
+    )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
